4dcf503bfa9101a73667c9a1c6d8d9424d696794
Francisfox <francismarfox@hotmail.com>
2022-12-30 23:36:42 +0000

    -

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -1,9 +1,10 @@
 
-#include <WebServer.h>
+#include <WebServer.h>                                //inclui a biblioteca webServer para acesso a rede wifi
+// baixar a Biblioteca no Download ZIP em https://github.com/Links2004/arduinoWebSockets
 #include <WebSocketsServer.h>                         //incluindo a class WebSocketsServer 
 
-const char* ssid = "wifi-ssid";
-const char* password = "wifi-password";
+const char* ssid = "wifi-ssid";                       //coloca o ssid da rede interna para acessar      
+const char* password = "wifi-password";               //coloca o password da rede interna para acessar      
 
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer (88);   // criando o objeto WebSocket para o WebSocketsServer 
