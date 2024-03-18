2b337b573167a667c83ab60436c497c17f9121ba
Francismar dos Santos Brito <79348772+Francisfox@users.noreply.github.com>
2024-03-18 18:52:17 -0300

    Update Botao.h
    -

--- webserver 1.1.2/webserver/Botao.h
+++ webserver 1.1.2/webserver/Botao.h
@@ -9,28 +9,46 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t weleng
   String payloadString = (const char *)payload;
   Serial.print ("payloadString = ");
   Serial.println(payloadString);
-  if (type == WStype_TEXT){ 
-    byte separator = payloadString.indexOf('=');
-    String var = payloadString.substring(0,separator);
-    Serial.print("var = ");
-    Serial.println(var);
-    String valor = payloadString.substring(separator+1);
-    Serial.print("valor = ");
-    Serial.println(valor);
-    Serial.println(" ");
-    
-    if(var == "LEDonoff"){
-      LEDonnff = false;
-      if(valor == "ON")LEDonnff = true;      
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        //String jsonResponse = "{ \"id\": \""+ deviceId +"\", \"type\": 1, \"value\": " + String(digitalRead(relayPin)) + " }";
+        //webSocket.broadcastTXT(jsonResponse); 
+      }
+      break;
+    case WStype_TEXT: {
+      byte separator = payloadString.indexOf('=');
+      String var = payloadString.substring(0,separator);
+      Serial.print("var = ");
+      Serial.println(var);
+      String valor = payloadString.substring(separator+1);
+      Serial.print("valor = ");
+      Serial.println(valor);
+      Serial.println(" ");
+      
+      if(var == "LEDonoff"){
+        LEDonnff = false;
+        if(valor == "ON")LEDonnff = true;      
+      }
+      if(var == "texto"){
+        lcd.clear();
+        lcd.setBacklight(HIGH);
+        lcd.setCursor(0,0);
+        lcd.print("Arduino e ESP32 ");
+        lcd.setCursor(0,1);
+        lcd.print(valor);
+      }
     }
-    if(var == "texto"){
-      lcd.clear();
-      lcd.setBacklight(HIGH);
-      lcd.setCursor(0,0);
-      lcd.print("Arduino e ESP32 ");
-      lcd.setCursor(0,1);
-      lcd.print(valor);
-    }
+      break;
+    case WStype_BIN:
+      Serial.printf("[%u] get binary length: %u\n", num, length);
+      hexdump(payload, length);
+      break;  
   }
 }  
                                           // parte do codigo que faz a contagem do tempo usando a função millis().
