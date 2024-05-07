95b7c66974a360b7cd76ac5ed3e3f56707dac213
Francismar dos Santos Brito <79348772+Francisfox@users.noreply.github.com>
2024-05-07 18:01:57 -0300

    Update

--- Grbl_Esp32/src/Grbl.cpp
+++ Grbl_Esp32/src/Grbl.cpp
@@ -1,7 +1,9 @@
 #include "Grbl.h"
 #include <WiFi.h>
 void grbl_init() {
-  
+  WiFi.persistent(false);       //Desativa o armazenamento permanente das configurações de WIFI
+  WiFi.disconnect(true);        //Desconecta imediatamente o dispositivo de uma rede WIFI
+  WiFi.enableSTA(false);        //Desabilita o modo de Station (STA) - não permitindo a opção de acessar redes existentes 
 }  
 void run_once() {
   
