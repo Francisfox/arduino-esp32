b445151b0ec5acd9167a784032729bf660066503
Francismar dos Santos Brito <79348772+Francisfox@users.noreply.github.com>
2024-05-25 16:08:48 -0300

    Create SAIDAS.cpp
    -

---
+++ WebServer_1/webserverAccessPoint/SAIDAS.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "SAIDAS.h"
+SAIDAS::SAIDAS(int pin){
+  this->_pin = pin;
+  this->_state = false;
+  pinMode(_pin, OUTPUT);
+}
+void SAIDAS::on(){
+  digitalWrite(_pin, HIGH);
+  _state = true;
+}
+void SAIDAS::off(){
+  digitalWrite(_pin, LOW);
+  _state = false
+}
+void SAIDAS::toggle(){
+  if(state){
+    off();  
+  }else{
+    on();
+  }
+}
+bool SAIDAS::isOn(){
+  return _state;
+}
